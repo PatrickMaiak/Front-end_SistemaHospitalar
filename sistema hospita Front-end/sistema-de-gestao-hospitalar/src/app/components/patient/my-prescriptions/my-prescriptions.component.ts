370c3de3e59e826b342a5463bd921383e48824b0
PatrickMaiak <patrickmaiakremer@gmail.com>
2025-04-17 11:19:34 -0300

    criação dos componentes

---
+++ sistema hospita Front-end/sistema-de-gestao-hospitalar/src/app/components/patient/my-prescriptions/my-prescriptions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-prescriptions',
+  imports: [],
+  templateUrl: './my-prescriptions.component.html',
+  styleUrl: './my-prescriptions.component.css'
+})
+export class MyPrescriptionsComponent {
+
+}
